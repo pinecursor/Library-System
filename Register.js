9b32d0f6374def66c97e38163a8a792e76412fac
Anubhab Banerjee <72434822+pinecursor@users.noreply.github.com>
2022-05-04 21:26:21 +0530

    First Commit

---
+++ Register.js
@@ -0,0 +1,6 @@
+secretCode = ()=>{
+    document.getElementById("Code").style.visibility="visible";
+  }
+secretCodeDisappear = ()=>{
+  document.getElementById("Code").style.visibility="hidden";
+}
